e110e63d5977865158e942c1d46c2130b9a2a1ec
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-01-23 15:35:03 +0800

    modified buttons

--- resources/views/livewire/admin/view-master-list-data.blade.php
+++ resources/views/livewire/admin/view-master-list-data.blade.php
@@ -99,28 +99,55 @@
                                     <tr>
                                         @if ($loop->first)
                                             <td class="whitespace-nowrap py-4 pr-3 text-sm font-medium text-gray-900 pl-2" rowspan="{{ $rowCount }}">
-                                                <div>
-                                                <x-button.circle class="mr-4" negative xs icon="trash" wire:click="deleteSubject({{$existingCase->id}})"/>
-                                                <x-button.circle class="mr-4" info xs label="+" wire:click="addExistingCaseData({{$existingCase->id}})"/>
+                                                <div class="flex">
+
+                                                    <button class="mr-4 p-0 flex justify-center align-middle" wire:click="deleteSubject({{$existingCase->id}})">
+                                                        <div class="flex flex-col items-center">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-600">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                              </svg>
+                                                          {{-- <small class="text-blue-600">Add Row</small> --}}
+                                                      </div>
+                                                      </button>
+                                                {{-- <x-button class="mr-4" negative xs icon="trash" wire:click="deleteSubject({{$existingCase->id}})"/> --}}
+                                                {{-- <x-button class="mr-4" info xs icon="plus" wire:click="addExistingCaseData({{$existingCase->id}})"/> --}}
+                                                    <button class="mr-4 p-0 flex justify-center align-middle" wire:click="addExistingCaseData({{$existingCase->id}})">
+                                                          <div class="flex flex-col items-center">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-blue-600">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                                              </svg>
+                                                            {{-- <small class="text-blue-600">Add Row</small> --}}
+                                                        </div>
+                                                        </button>
                                                 {{ strtoupper($existingCase->subject) }}
                                                 </div>
                                             </td>
                                         @endif
                                         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                            <div class="flex space-x-2">
+                                            <div class="flex space-x-5">
 
                                                 @if($otherData->count() > 1)
                                                 <button class="p-0 flex justify-center align-middle" wire:click="deleteExistingCaseData({{$value->id}})">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-red-600">
+                                                    {{-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-red-600">
                                                         <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" />
-                                                      </svg>
+                                                      </svg> --}}
+                                                      <div class="flex flex-col items-center">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-600">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                          </svg>
+                                                        <small class="text-red-600">Delete Row</small>
+                                                    </div>
+
+
                                                     </button>
                                                 @endif
                                                 <button class="p-0 flex justify-center align-middle" wire:click="showExistingCaseData({{$value->id}})">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  fill="currentColor" class="w-5 h-5 text-green-600 cursor-pointer">
-                                                        <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
-                                                        <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
-                                                      </svg>
+                                                    <div class="flex flex-col items-center">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green-600">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                          </svg>
+                                                        <small class="text-green-600">Edit Row</small>
+                                                    </div>
                                                     </button>
                                             </div>
 
